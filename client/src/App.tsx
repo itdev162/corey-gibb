a1d5f487eb5f9de0f18219122c24bb2e5982cb6c
Corey Gibb <coreygibb@Coreys-iMac.local>
2021-10-02 14:27:54 -0500

    Completed Activity 4

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends React.Component {
-    render() {
-      return (
-        <div className="App">
-          <header className="App-header">
-            Blogbox
-          </header>
-        </div>
-      );
-    }
+
+  state = {
+    values: []
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/api/values')
+    .then((response) => {
+      this.setState({
+        values: response.data
+      })
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          Blogbox
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
